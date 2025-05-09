c1d548352a0389663b88d87785889e4ab5df83de
Nursultan <nursultandaurenbek6@gmail.com>
2025-05-09 17:51:16 +0800

    janartuu

--- src/main/java/labxx/sict/must/edu/mn/Multiplication.java
+++ src/main/java/labxx/sict/must/edu/mn/Multiplication.java
@@ -1,6 +1,10 @@
 package labxx.sict.must.edu.mn;
-public class Multiplication { 
-    public double multiply(double a, double b) { 
-    return a * b; 
-    } 
-    } 
+
+public class Multiplication {
+    public double multiply(double a, double b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return a * b;
+    }
+}
--- src/test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
+++ src/test/java/labxx/sict/must/edu/mn/MultiplicationTest.java
@@ -1,10 +1,10 @@
 package labxx.sict.must.edu.mn;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MultiplicationTest {
-
     @Test
     void testMultiplyPositive() {
         Multiplication calc = new Multiplication();
@@ -12,8 +12,9 @@ public class MultiplicationTest {
     }
 
     @Test
-    void testMultiplyNegative() {
+    void testMultiplyByZero() {
         Multiplication calc = new Multiplication();
-        assertEquals(-6.0, calc.multiply(-2.0, 3.0));
+        assertEquals(0.0, calc.multiply(0.0, 5.0));
+        assertEquals(0.0, calc.multiply(7.0, 0.0));
     }
 }
